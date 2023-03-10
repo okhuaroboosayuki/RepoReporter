4b84321f0e2863bbbe39faab23c42a7abd5c288d
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-11 00:24:54 +0100

    created Loader component

---
+++ src/components/LoaderSpin.vue
@@ -0,0 +1,14 @@
+<template>
+  <flower-spinner :animation-duration="2500" :size="70" color="#ff1d5e" />
+</template>
+
+<script>
+import { FlowerSpinner } from "epic-spinners";
+export default {
+  name: "LoaderSpin",
+
+  components: {
+    FlowerSpinner,
+  },
+};
+</script>
