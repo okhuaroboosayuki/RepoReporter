7509651d1d972f8d14851d4fd4db43d2320f0d32
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-10 21:41:13 +0100

    added contents to landing page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="homepage">
+    <div class="hero_wrapper">
+      <h1 class="hero_header">Welcome to <span>RepoReporter</span></h1>
+      <p class="hero_text">
+        where my GitHub repositories go to have a party without me! My web app is like the
+        cool kids' table at lunch, except it's only for my code. So kick back, relax, and
+        let RepoReporter do the heavy lifting of keeping you up to date on all my
+        repositories' shenanigans.
+      </p>
+      <button class="call_to_action_btn">Track My Code, Stat!</button>
+    </div>
   </div>
 </template>
