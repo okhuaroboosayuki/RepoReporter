95d7b8853d8d4f1f97276089d450f8fafa835a0b
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-10 23:40:53 +0100

    added navigation link to repository page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,12 +4,14 @@
       <!-- do a text change animation -->
       <h1 class="hero_header">Welcome to <span>RepoReporter</span></h1>
       <p class="hero_text">
-        where my GitHub repositories go to have a party without me! My web app is like the
-        cool kids' table at lunch, except it's only for my code. So kick back, relax, and
-        let RepoReporter do the heavy lifting of keeping you up to date on all my
+        where my GitHub repositories go to have a party without me! This web app is like
+        the cool kids' table at lunch, except it's only for my code. So kick back, relax,
+        and let RepoReporter do the heavy lifting of keeping you up to date on all my
         repositories' shenanigans.
       </p>
-      <button class="call_to_action_btn">Track My Code, Stat!</button>
+      <router-link to="/repositories">
+        <button class="call_to_action_btn">Check Out My Repos</button>
+      </router-link>
     </div>
   </div>
 </template>
