aeff8c350ddd794efe2d6ec7c839be13627a020f
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-10 23:42:56 +0100

    added nav header

---
+++ src/views/RepoView.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderNav />
+  <div>
+    <h2>Repos page</h2>
+  </div>
+</template>
+
+<script>
+import HeaderNav from "../components/HeaderNav.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    HeaderNav,
+  },
+};
+</script>
