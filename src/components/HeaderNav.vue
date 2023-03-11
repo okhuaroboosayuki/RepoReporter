f268932961ab1eb9d19613f05811f78a09a4d0b2
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-12 00:11:53 +0100

    added mobile menu

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,30 +1,58 @@
 <template>
-  <div class="nav_wrapper">
+  <nav class="nav_wrapper">
     <div class="logo">
       <router-link to="/">RepoReporter</router-link>
     </div>
-    <nav>
+    <div class="navigation">
       <ul>
         <li>
           <router-link to="/">Home</router-link>
         </li>
 
         <li>
-          <router-link to="/">Repositories</router-link>
+          <router-link to="/repos">Repositories</router-link>
         </li>
       </ul>
-    </nav>
-  </div>
+    </div>
+    <div class="mobile_menu">
+      <nav>
+        <div class="menu_icon" @click="showMenu = !showMenu">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+        </div>
+        <div class="menu" v-if="showMenu">
+          <div class="close_button" @click="showMenu = !showMenu"></div>
+          <ul>
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/repos">Repositories</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const showMenu = ref(false);
+    return { showMenu };
+  },
+};
 </script>
 
 <style scoped>
 a {
   color: var(--light-shade);
 }
+
 .nav_wrapper {
   display: flex;
   align-items: center;
@@ -39,40 +67,140 @@ a {
   font-family: var(--logo-font);
 }
 
-nav ul {
+div ul {
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 0.5rem;
 }
 
-nav ul li {
+div ul li {
   list-style: none;
   margin: 0 1rem;
   font-family: var(--title-font);
 }
 
-nav ul li a {
+div ul li a {
   font-size: 1.2rem;
 }
 
-nav ul li a:hover {
+div ul li a:hover {
   color: var(--btn-color);
 }
 
-@media (max-width: 420px) {
-  nav ul {
-    flex-direction: column;
+.router-link-exact-active {
+  color: var(--btn-hover);
+}
+
+.mobile_menu {
+  display: none;
+}
+
+.menu_icon {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 20px;
+  cursor: pointer;
+}
+
+.bar {
+  width: 25px;
+  height: 3px;
+  background-color: var(--btn-color);
+  transition: transform 0.3s;
+}
+
+.bar:nth-child(2) {
+  margin: 5px 0;
+}
+
+.menu {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 50vh;
+  background-color: var(--dark-shade);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.menu ul {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+}
+
+.menu ul li {
+  margin: 1rem 0;
+  font-family: var(--title-font);
+}
+
+@media (max-width: 600px) {
+  .logo{
+    position: relative;
+    right: 60px;
+  }
+  .navigation {
+    display: none;
   }
 
-  nav ul li {
-    margin: 0.5rem 0;
+  .mobile_menu {
+    display: flex;
   }
 
-  nav ul {
-    padding: 10px;
-    border: 1px solid var(--light-shade);
-    border-radius: 5px;
+  .menu_icon{
+    position: relative;
+    left: 70px;
+  }
+
+  .close_button {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    align-self: flex-end;
+    right: 47px;
+    top: -92px;
+  }
+
+  .close_button::before,
+  .close_button::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 22px;
+    height: 2px;
+    background-color: var(--btn-color);
+  }
+
+  .close_button::before {
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
+
+  .close_button::after {
+    transform: translate(-50%, -50%) rotate(-45deg);
+  }
+}
+
+@media (max-width: 450px) {
+  .menu_icon{
+    position: relative;
+    left: 30px;
+  }
+  .close_button {
+    position: relative;
+    right: 40px;
+    top: -107px;
   }
 }
 </style>
