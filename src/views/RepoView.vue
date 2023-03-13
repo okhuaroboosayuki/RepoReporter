fa72324f29ff4a0ef053f8eb9f24401e8cdd2e79
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-13 12:31:30 +0100

    called MyRepos component

--- src/views/RepoView.vue
+++ src/views/RepoView.vue
@@ -1,17 +1,20 @@
 <template>
   <HeaderNav />
+  <h2>Repos page</h2>
   <div>
-    <h2>Repos page</h2>
+    <MyRepos />
   </div>
 </template>
 
 <script>
 import HeaderNav from "../components/HeaderNav.vue";
+import MyRepos from "../components/MyRepos.vue";
 
 export default {
   name: "HomePage",
   components: {
     HeaderNav,
+    MyRepos,
   },
 };
 </script>
