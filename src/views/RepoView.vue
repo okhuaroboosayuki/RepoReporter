c0c1415e5f8a2ebd756dc87848ddeba03405c2ce
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-14 23:10:30 +0100

    displayed my profile component and added some styling

--- src/views/RepoView.vue
+++ src/views/RepoView.vue
@@ -1,20 +1,34 @@
 <template>
   <HeaderNav />
-  <h2>Repos page</h2>
-  <div>
-    <MyRepos />
+  <div class="repo_wrapper">
+    <div>
+      <MyProfile />
+    </div>
+    <div>
+      <MyRepos />
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderNav from "../components/HeaderNav.vue";
 import MyRepos from "../components/MyRepos.vue";
+import MyProfile from "../components/MyProfile.vue";
 
 export default {
   name: "HomePage",
   components: {
     HeaderNav,
     MyRepos,
+    MyProfile,
   },
 };
 </script>
+
+<style scoped>
+.repo_wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem;
+}
+</style>
