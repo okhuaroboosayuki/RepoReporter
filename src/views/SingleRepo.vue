44af44b5ffb8d530c84cfdbb577002c778e81d10
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-15 18:41:00 +0100

    imported HeaderNav component

--- src/views/SingleRepo.vue
+++ src/views/SingleRepo.vue
@@ -1,5 +1,16 @@
 <template>
+  <HeaderNav />
   <div>
     <h3>Single Repo</h3>
   </div>
 </template>
+
+<script>
+import HeaderNav from "../components/HeaderNav.vue";
+
+export default {
+  components: {
+    HeaderNav,
+  },
+};
+</script>
