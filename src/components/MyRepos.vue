efd35d11669c19a6a51eecae7fb00c9782ae7ed8
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-17 12:54:25 +0100

    added repo topics card, added paginated back instead of importing it, & added responsiveness

--- src/components/MyRepos.vue
+++ src/components/MyRepos.vue
@@ -10,8 +10,13 @@
             }}</router-link>
           </h3>
         </div>
+
         <div class="repo_desc">
           <p>{{ repo.description ? repo.description : "no description" }}</p>
+        </div>
+
+        <div class="repo_topics">
+          <span class="topic" v-for="topic in repo.topics" :key="topic">{{ topic }}</span>
         </div>
 
         <div class="view_more">
@@ -21,22 +26,67 @@
         </div>
       </li>
     </ul>
+
     <!-- pagination -->
-    <PaginatedButtons />
+    <div class="pagination">
+      <div class="first_set_btn">
+        <button
+          @click="setPage(1)"
+          :disabled="repos.currentPage === 1"
+          :class="repos.currentPage === 1 ? 'disabled' : 'first_btn'"
+        >
+          First
+        </button>
+        <button
+          @click="setPage(repos.currentPage - 1)"
+          :disabled="repos.currentPage === 1"
+          :class="repos.currentPage === 1 ? 'disabled' : 'prev_btn'"
+        >
+          Prev
+        </button>
+      </div>
+
+      <button
+        v-for="button in pageButtons"
+        :key="button.number"
+        @click="setPage(button.number)"
+        :disabled="button.active"
+        :class="button.active ? 'active_btn' : 'normal_btn'"
+      >
+        {{ button.number }}
+      </button>
+
+      <div class="last_set_btn">
+        <button
+          @click="setPage(repos.currentPage + 1)"
+          :disabled="repos.currentPage === parseInt(repos.totalRepos)"
+          :class="
+            repos.currentPage === parseInt(repos.totalRepos) ? 'disabled' : 'next_btn'
+          "
+        >
+          Next
+        </button>
+        <button
+          @click="setPage(parseInt(repos.totalRepos))"
+          :disabled="repos.currentPage === parseInt(repos.totalRepos)"
+          :class="
+            repos.currentPage === parseInt(repos.totalRepos) ? 'disabled' : 'last_btn'
+          "
+        >
+          Last
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import PaginatedButtons from "./PaginatedButtons.vue";
 import { computed } from "vue";
 import useRepos from "../composables/useRepos.js";
 
 export default {
-  components: {
-    PaginatedButtons,
-  },
   setup() {
-    const { repos } = useRepos();
+    const { repos, setPage, pageButtons } = useRepos();
 
     // getClass replace spaces with underscores and add "repo_" prefix
     const getClass = computed(() => (name) => {
@@ -46,6 +96,8 @@ export default {
     return {
       repos,
       getClass,
+      setPage,
+      pageButtons,
     };
   },
 };
@@ -68,8 +120,6 @@ export default {
 }
 
 .repo_list {
-  display: flex;
-  flex-direction: column;
   padding: 30px;
 }
 
@@ -83,6 +133,7 @@ export default {
 .repo_vue-composable {
   align-self: flex-end;
   left: 20rem;
+  max-width: 30rem;
 }
 
 .repo_altschool-react,
@@ -112,6 +163,7 @@ li {
   border-radius: 10px;
   animation: glowing 1500ms infinite, slideInLeft 1.5s ease-in-out;
   padding: 20px 30px;
+  margin-bottom: 1rem;
   position: relative;
 }
 
@@ -134,6 +186,23 @@ li h3 {
   font-size: 0.9rem;
 }
 
+.repo_topics {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.topic {
+  background-color: var(--dark-shade);
+  color: var(--light-shade);
+  padding: 5px 10px;
+  margin-top: 0.5rem;
+  border: 0.5px solid var(--light-shade);
+  border-radius: 5px;
+  margin-right: 5px;
+  font-size: 0.8rem;
+}
+
 .view_more {
   align-self: flex-end;
 }
@@ -144,6 +213,7 @@ li h3 {
   outline: none;
   border: 0.3px solid var(--light-shade);
   padding: 15px;
+  margin-top: 1rem;
   font-size: 0.9rem;
   text-transform: capitalize;
   cursor: pointer;
@@ -162,5 +232,102 @@ li h3 {
 
 button a:hover {
   color: var(--dark-shade);
+}
+
+.pagination {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 1rem 0;
+}
+
+.pagination button {
+  padding: 10px 15px;
+  margin-left: 7px;
+  cursor: pointer;
+  outline: none;
+  background-color: transparent;
+  border: 0.5px solid var(--btn-hover);
+  color: var(--light-shade);
+}
+
+.normal_btn:hover {
+  background-color: var(--btn-hover);
+  color: var(--light-shade);
+}
+
+.disabled {
+  background-color: grey !important;
+  cursor: not-allowed !important;
+}
+
+.first_btn,
+.prev_btn,
+.next_btn,
+.last_btn,
+.active_btn {
+  background-color: var(--light-shade) !important;
+  color: var(--dark-shade) !important;
+}
+
+.first_btn:hover,
+.prev_btn:hover,
+.next_btn:hover,
+.last_btn:hover {
+  background-color: var(--dark-shade) !important;
+  color: var(--light-shade) !important;
+}
+
+.first_set_btn,
+.last_set_btn {
+  display: flex;
+  gap: 3px;
+}
+
+.first_set_btn {
+  margin-right: 3px;
+}
+
+.last_set_btn {
+  margin-left: 3px;
+}
+
+@media all and (max-width: 1180px) {
+  .repo_altschool-opensource-names,
+  .repo_Blog-App,
+  .repo_counter-app,
+  .repo_okhuarobo-osayuki-assignment-02,
+  .repo_okhuaroboosayuki,
+  .repo_react-calculator,
+  .repo_React-Router,
+  .repo_vue-composable {
+    align-self: center;
+    left: 0rem;
+  }
+
+  .repo_altschool-react,
+  .repo_Entre-Konnect,
+  .repo_QR-code-component,
+  .repo_RepoReporter {
+    align-self: center;
+    right: 0rem;
+  }
+
+  .repo_list {
+    padding: 0;
+    gap: 1.2rem;
+  }
+}
+
+@media all and (max-width: 680px) {
+  .repo_list {
+    padding: 0;
+    gap: 1.2rem;
+  }
+
+  .next_btn,
+  .last_btn,
+  .last_set_btn .disabled {
+    margin-top: 0.5rem;
+  }
 }
 </style>
