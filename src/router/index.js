f9c5672c7e384003fc7155b7e8b7515832173fb0
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-17 12:57:18 +0100

    lazy loaded commits, languages, created, license, & notfound views; & added children routes to SingleRepo route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const HomePage = () => import(/* webpackChunkName: "HomePage" */ '@/views/HomePage.vue')
 const Repositories = () => import(/* webpackChunkName: "Repositories" */ '@/views/RepoView.vue')
 const SingleRepo = () => import(/* webpackChunkName: "SingleRepo" */ '@/views/SingleRepo.vue')
+const Commits = () => import(/* webpackChunkName: "Commits" */ '@/views/RepoPages/RepoCommit.vue')
+const Languages = () =>
+  import(/* webpackChunkName: "Languages" */ '@/views/RepoPages/RepoLanguage.vue')
+const Created = () => import(/* webpackChunkName: "Created" */ '@/views/RepoPages/RepoCreated.vue')
+const License = () => import(/* webpackChunkName: "License" */ '@/views/RepoPages/RepoLicense.vue')
+const NotFound = () => import(/* webpackChunkName: "NotFound" */ '@/views/NotFound.vue')
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,12 +21,39 @@ const router = createRouter({
     {
       path: '/repos',
       name: 'Repos',
-      component: Repositories,
+      component: Repositories
     },
     {
       path: '/repos/:repo',
       name: 'Repo',
-      component: SingleRepo
+      component: SingleRepo,
+      children: [
+        {
+          path: '/repos/:repo/commits',
+          name: 'Commits',
+          component: Commits
+        },
+        {
+          path: '/repos/:repo/languages',
+          name: 'Language',
+          component: Languages
+        },
+        {
+          path: '/repos/:repo/createdAt',
+          name: 'Created At',
+          component: Created
+        },
+        {
+          path: '/repos/:repo/license',
+          name: 'License',
+          component: License
+        }
+      ]
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
