8e736ab3f2f3377fe4a29fa5f83dc6e175fe4ceb
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-18 02:49:20 +0100

    imported import useAnimatedTypewriter from ../composables/useAnimatedTypewriter;

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,10 @@
   <div class="homepage">
     <div class="hero_wrapper">
       <!-- do a text change animation -->
-      <h1 class="hero_header">Welcome to <span>RepoReporter</span></h1>
+      <h1 class="hero_header">
+        Welcome to <span class="animated_text">{{ typeValue }}</span
+        ><span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
+      </h1>
       <p class="hero_text">
         where my GitHub repositories go to have a party without me! This web app is like
         the cool kids' table at lunch, except it's only for my code. So kick back, relax,
@@ -15,3 +18,18 @@
     </div>
   </div>
 </template>
+
+<script>
+import useAnimatedTypewriter from "../composables/useAnimatedTypewriter";
+
+export default {
+  setup() {
+    const { typeValue, typeStatus } = useAnimatedTypewriter();
+
+    return {
+      typeValue,
+      typeStatus,
+    };
+  },
+};
+</script>
