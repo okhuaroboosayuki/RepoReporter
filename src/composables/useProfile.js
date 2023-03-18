9624b47e0631e64823d4b6dfe73bfb1018753b17
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-03-18 19:15:48 +0100

    added profileUrl key into the profile object

--- src/composables/useProfile.js
+++ src/composables/useProfile.js
@@ -3,6 +3,7 @@ import {  onMounted, reactive, watchEffect,  } from 'vue'
 export default function useProfile() {
         const profile = reactive({
             name: '',
+            profileUrl: '',
             bio: '',
             avatarUrl: '',
             followers: '',
@@ -29,6 +30,7 @@ export default function useProfile() {
             profile.followers = data.followers;
             profile.following = data.following;
             profile.publicRepos = data.public_repos;
+            profile.profileUrl = data.html_url;
 
         }
 
@@ -42,40 +44,3 @@ export default function useProfile() {
             profile
         }
 }
-
-
-// const profile = reactive({
-//     name: '',
-//     bio: '',
-//     avatar_url: '',
-//     followers: '',
-//     following: '',
-//     location: '',
-//     public_repos: '',
-//     created_at: '',
-//     updated_at: '',
-//   })
-
-//     const fetchProfile = async () => {
-//         const username = import.meta.env.VITE_GITHUB_USERNAME;
-//         const url = `https://api.github.com/users/${username}`;
-//         const token = import.meta.env.VITE_GITHUB_TOKEN;
-
-//         const response = await fetch(url, {
-//             headers: {
-//                 'Authorization': `token ${token}`
-//             }
-//         })
-
-//         const data = await response.json();
-
-        // profile.name = data.name;
-        // profile.bio = data.bio;
-        // profile.avatar_url = data.avatar_url;
-        // profile.followers = data.followers;
-        // profile.following = data.following;
-        // profile.location = data.location;
-        // profile.public_repos = data.public_repos;
-        // profile.created_at = data.created_at;
-        // profile.updated_at = data.updated_at;
-//     }
